192532bcb3f6c14e4cb7fbda9cc0f8282f92ddb6
cjwd <chinarakinda@gmail.com>
2014-03-23 23:14:02 -0400

    Add product partial template

---
+++ product-partial.php
@@ -0,0 +1,22 @@
+<footer class="product-footer product-tiers">
+    <div class="grid">
+        <div class="grid__item one-whole desk-one-half">
+            <div class="product-tier first">
+                <div class="grid__item one-half">
+                    <li><?php echo $data['pricing_tier_group']; ?></li>
+                    <h3 class="product-tier-name"></h3>
+                    <span class="tier-price"></span>
+                    <a href="">View Example</a>
+                </div><!--
+                --><div class="grid__item one-half">
+                    <ul class="check-list">
+                        <li>Feature 1</li>
+                        <li>Feature 2</li>
+                        <li>Feature 3</li>
+                    </ul>
+                </div>
+            </div> <!-- end product-tier -->
+        </div><!--
+    -->
+    </div> <!-- end grid row -->
+</footer>

04fe4d4ac163e8b8fbb6abc2fb4a303e8b52f2e3
cjwd <chinarakinda@gmail.com>
2014-04-28 02:25:41 -0400

    Remove excerpt from portfolio, add tagline

--- parts/meta-boxes/cjwd-portfolio-details.php
+++ parts/meta-boxes/cjwd-portfolio-details.php
@@ -3,39 +3,47 @@
  * Title: Portfolio Details
  * Post Type: cjwd_portfolio
  */
+piklist( 'field', array(
+		'type' => 'textarea'
+		,'field' => 'project_tagline'
+ 		,'label' => __('Tagline')
+ 		,'description' => __('No more than 70 characters including spaces')
+ 		,'attributes' => array(
+   			'class' => 'text'
+ 			)
+	));
+/*piklist('field', array(
+		'type' => 'editor'
+		,'field' => 'project_excerpt'
+		,'label' => __('Excerpt')
+		,'description' => __('Short summary of project')
+		,'value' => 'Write a short summary of the project here'
+		,'template' => 'field'
+		,'options' => array (
+			'wpautop' => true
+			,'media_buttons' => false
+			,'tabindex' => ''
+			,'editor_css' => ''
+			,'editor_class' => ''
+			,'teeny' => false
+			,'dfw' => false
+			,'tinymce' => true
+			,'quicktags' => true
+		)
+	));*/
 
 piklist('field', array(
-    'type' => 'editor'
-    ,'field' => 'project_excerpt'
-    ,'label' => __('Excerpt')
-    ,'description' => __('Short summary of project')
-    ,'value' => 'Write a short summary of the project here'
-    ,'template' => 'field'
-    ,'options' => array (
-      'wpautop' => true
-      ,'media_buttons' => false
-      ,'tabindex' => ''
-      ,'editor_css' => ''
-      ,'editor_class' => ''
-      ,'teeny' => false
-      ,'dfw' => false
-      ,'tinymce' => true
-      ,'quicktags' => true
-    )
-  ));
+		'type' => 'text'
+		,'field' => 'project_client_name'
+		,'label' => __('Client', 'cjwd-post-types')
+		,'columns' => '12'
+	));
 
 piklist('field', array(
-    'type' => 'text'
-    ,'field' => 'project_client_name'
-    ,'label' => __('Client', 'cjwd-post-types')
-    ,'columns' => '12'
-  ));
-
-piklist('field', array(
-    'type' => 'text'
-    ,'field' => 'project_url'
-    ,'label' => __('Project URL', 'cjwd-post-types')
-    ,'description' => __('URL to launch the project')
-    ,'value' => 'http://'
-    ,'columns' => '12'
-  ));
+		'type' => 'text'
+		,'field' => 'project_url'
+		,'label' => __('Project URL', 'cjwd-post-types')
+		,'description' => __('URL to launch the project')
+		,'value' => 'http://'
+		,'columns' => '12'
+	));
